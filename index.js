5567bc97706a1075cf430e5ec73f4da1599b8a81
Nishant <nishant.joshi1109@gmail.com>
2022-12-13 15:39:27 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -2,10 +2,54 @@ const express = require('express');
 const http  = require('http');
 const hostname= 'localhost';
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const port = 4000;
 
 const app =express();
+
+app.use(bodyParser.json());
 app.use(morgan('dev'));
+
+app.all('/dishes', (req, res, next)=>{
+   res.statusCode = 200;
+   res.setHeader('Content-Type', 'text/plain');
+   next();
+});
+
+app.get('/dishes', (req, res, next)=>{
+    res.end('Return name of dishes');
+})
+
+app.post('/dishes', (req,  res, next)=>{
+    res.end('Name of Dish: ' + req.body.name + '\n' +'With description: ' + req.body.des)
+})
+
+app.put('/dishes', (req,  res, next)=>{
+    res.statusCode = 403
+    res.end('put Operation not supported on endpoint')
+})
+
+app.delete('/dishes', (req,  res, next)=>{
+    res.end('Deleting all data')
+})
+
+app.get('/dishes/:dishid', (req, res, next)=>{
+    res.end('Sending details of dish '+ req.params.dishid + 'to you'+ '\n' + 'With name: ' + req.body.name+ '\n'+'Descritpion: ' + req.body.des);
+})
+
+app.post('/dishes', (req,  res, next)=>{
+    res.end('Post not supported on this endpoint - /dishes/' + req.params.dishid)
+})
+
+app.put('/dishes/:dishid', (req,  res, next)=>{
+    
+    res.write('Updating dish: ' + req.params.dishid + '\n')
+    res.end("Will update dish: " + req.body.name + '\n' +'Description' + req.body.des) 
+})
+
+app.delete('/dishes/:dishid', (req,  res, next)=>{
+    res.end('Deleting dish no. ' + req.params.dishid) 
+})
 
 app.use(express.static(__dirname + '/public'))
 
