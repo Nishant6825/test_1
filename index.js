697a11a2f6e9ab70a07ea2adf4e460af73f913c3
Nishant <nishant.joshi1109@gmail.com>
2022-12-13 12:05:17 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const http  = require('http');
 const hostname= 'localhost';
+const morgan = require('morgan')
 const port = 4000;
 
 const app =express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'))
+
 app.use((req, res, next)=>{
-    console.log(req.headers);
+    // console.log(req.headers);
+
     res.statusCode =200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is Express Server</h1></body></html>')
